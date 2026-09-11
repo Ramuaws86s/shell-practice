1c7e8ef3a28943b7a8473024b0968229652b5930
ramu <ramu.koti9177@gmail.com>
2026-09-11 22:26:19 +0530

    variables

--- 8.special-var.sh
+++ 8.special-var.sh
@@ -3,3 +3,6 @@
 echo "All variables passed to the script: $@"
 echo "All variables passed to the script: $*"
 echo "script name: $0"
+echo "current directory: $PWD"
+echo "who is running this: $USER"
+echo "home directory of user: $HOME"
