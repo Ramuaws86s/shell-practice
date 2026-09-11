6babdf6fd742b6337860ecdc5c1041bb7fc0070c
ramu <ramu.koti9177@gmail.com>
2026-09-11 16:41:23 +0530

    some message

--- 3.Variables.sh
+++ 3.Variables.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 # No space before or after equal
-PERSON1=John
-PERSON2=Raheem
+PERSON1=kohli
+PERSON2=Rohit
 
 echo "$PERSON1:: Hi $PERSON2, How are you?"
 echo "$PERSON2:: Hello $PERSON1, Iam fine. How are you?"
