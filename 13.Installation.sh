48bf4370b8a8fb88e62743a29a25199e98524ff4
ramu <ramu.koti9177@gmail.com>
2026-09-12 15:04:30 +0530

    session06

---
+++ 13.Installation.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+USERID=$(id -u)
+
+if [ $USERID -n 0 ]; then
+   echo "ERROR:: please run this script with root privelege"
+   exit 1 # failure is other then 0
+fi
+
+VALIDATE(){ # functions receive inputs throught args just like shell script args
+    if [ $? -ne 0 ]; then
+       echo "ERROR:: Installing MySQL is failure"
+       exit 1
+    else
+       echo "Installing MySQL is SUCCESS"
+    fi
+}
+
+dnf install mysql -y
+VALIDATE $? "MySQL"
+
+dnf install nginx -y
+VALIDATE $? "Nginx"
+
+dnf install mongodb-mongosh -y
+VALIDATE $? "mongosh"
+ 
